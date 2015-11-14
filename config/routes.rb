34ccd80f63698ee755a82fa3d99aa96a93477389
Sean <sean.kerman@gmail.com>
2015-11-14 14:24:57 -0700

    added pages controller with home, about, contact, privacy, and terms views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/terms'
+
+  get 'pages/privacy'
+
   get 'courses/delete'
 
   get 'courses/edit'
