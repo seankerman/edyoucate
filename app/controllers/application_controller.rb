340d6c8bca7312c27497aeae63a1b74a591b89e2
Sean <sean.kerman@gmail.com>
2015-11-26 12:42:09 -0700

    home page now gives logout option when logged in. Aslo routs to user home page rather than creat account when logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,17 +15,4 @@ class ApplicationController < ActionController::Base
   	end
   end
 
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-  end
-
-  def user_signed_in?
-    unless session[:user_id]
-      return false
-    else
-      return true
-    end
-  end
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,17 @@ module ApplicationHelper
   	render(:partial => 'application/error_messages', :locals => {:object => object})
   end
 
-  
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def user_signed_in?
+    unless session[:user_id]
+      return false
+    else
+      return true
+    end
+  end
 
 end
